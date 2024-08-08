0ee36b9a634ad416cdf104527aa52bfb21e59754
Richard E Barber <kd6kxr@gmail.com>
2024-08-08 17:24:44 +0700

    Update macosx_bundle.sh

--- tools/osx/macosx_bundle.sh
+++ tools/osx/macosx_bundle.sh
@@ -429,7 +429,7 @@ function CreateDmg {
         echo "Building Fancy .dmg"
         MESSAGE="$(cat message)"
         magick ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png -pointsize 80 -fill Black -draw "text 14,1307 '${PROJECT_FULL_VERSION}'" -fill Salmon -draw "text 10,1300 '${PROJECT_FULL_VERSION}'" ./rtdmg-bkgd.png
-        magick ./rtdmg-bkgd.png -pointsize 100 -fill Black -gravity center -draw "text 24,606 \"$MESSAGE\"" -fill Red -gravity center -draw "text 20,600 \"$MESSAGE\"" ./rtdmg-bkgd.png
+        magick ./rtdmg-bkgd.png -pointsize 90 -fill Black -gravity center -font Menlo-Bold -draw "text 5,120 \"$MESSAGE\"" -fill Red -gravity center -font Menlo-Bold -draw "text 1,124 \"$MESSAGE\"" ./rtdmg-bkgd.png
         create-dmg \
         --background ./rtdmg-bkgd.png \
         --volname ${PROJECT_NAME}_${PROJECT_FULL_VERSION} \
