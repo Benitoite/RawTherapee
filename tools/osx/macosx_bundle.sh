e148ab63224b33113c84c627424074cd3739ebe3
Richard E Barber <kd6kxr@gmail.com>
2025-07-21 22:36:46 -0700

    Update macosx_bundle.sh

--- tools/osx/macosx_bundle.sh
+++ tools/osx/macosx_bundle.sh
@@ -269,10 +269,13 @@ done
 msg "Build GTK3 databases:"
 mkdir -p "${RESOURCES}"/share/gtk-3.0
 mkdir -p "${ETC}"/gtk-3.0
+
 # Change a relative path for the SVG pixbufloader
 install_name_tool -delete_rpath @loader_path/../lib "${LIB}"/libpixbufloader_svg.so
-install_name_tool -add_rpath "${PWD}"/"${LIB}" "${LIB}"/libpixbufloader_svg.so
+install_name_tool -change @rpath/librsvg-2.2.dylib "${PWD}"/"${LIB}"/librsvg-2.2.dylib  "${LIB}"/libpixbufloader_svg.so
+
 otool -l "${LIB}"/libpixbufloader_svg.so
+
 "${LOCAL_PREFIX}"/bin/gdk-pixbuf-query-loaders "${LIB}"/libpixbufloader*.so > "${ETC}"/gtk-3.0/gdk-pixbuf.loaders
 "${LOCAL_PREFIX}"/bin/gtk-query-immodules-3.0 "${LIB}"/im-* > "${ETC}"/gtk-3.0/gtk.immodules || "${LOCAL_PREFIX}"/bin/gtk-query-immodules "${LIB}"/im-* > "${ETC}"/gtk-3.0/gtk.immodules
 sed -i.bak -e "s|${PWD}/RawTherapee.app/Contents/|/Applications/RawTherapee.app/Contents/|" "${ETC}"/gtk-3.0/gdk-pixbuf.loaders "${ETC}/gtk-3.0/gtk.immodules"
@@ -282,6 +285,9 @@ rm "${ETC}"/*/*.bak
 
 # Remove a relative path for the SVG pixbufloader
 install_name_tool -change @rpath/librsvg-2.2.dylib /Applications/RawTherapee.app/Contents/Frameworks/librsvg-2.2.dylib RawTherapee.app/Contents/Frameworks/libpixbufloader_svg.so
+
+# Modify the libpixbufloader_svg librsvg install_name
+install_name_tool -change "${PWD}"/"${LIB}"/librsvg-2.2.dylib /Applications/"${LIB}"/librsvg-2.2.dylib "${LIB}"/libpixbufloader_svg.so
 
 # Install names
 ModifyInstallNames 2>/dev/null
